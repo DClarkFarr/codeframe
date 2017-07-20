45f7a2d1786b483fa5c47fc9c9ed3adf66691b56
Daniel Farr <dfarr@dhcp125.office.inetz.com>
2017-07-20 08:09:43 -0600

    fix autoload order

--- autoload.php
+++ autoload.php
@@ -1,7 +1,8 @@
 <?php 
 
 include __DIR__ . '/functions/autoload.php';
-include __DIR__ . '/classes/autoload.php';
 include __DIR__ . '/traits/autoload.php';
+include __DIR__ . '/classes/autoload.php';
+
 
 
